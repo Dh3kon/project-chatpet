4415e54ed2b4f1c5b9d2640e46711a166ba02196
Kleber Silva <kb-max@hotmail.com>
2018-12-02 16:49:54 -0200

    Controller de cliente criado

---
+++ src/main/java/com/chatpet/projectchatpet/controller/ClienteController.java
@@ -0,0 +1,49 @@
+package com.chatpet.projectchatpet.controller;
+
+import com.chatpet.projectchatpet.exception.RecursoNaoEncontradoException;
+import com.chatpet.projectchatpet.model.Cliente;
+import com.chatpet.projectchatpet.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @GetMapping("/clientes")
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+
+    @PostMapping("/clientes")
+    public Cliente createCliente(@Valid @RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/clientes/{clienteId}")
+    public Cliente updateCliente(@PathVariable("clienteId") Long clienteId, @Valid @RequestBody
+            Cliente clienteRequest) {
+        return clienteRepository.findById(clienteId).map(cliente -> {
+            cliente.setNome(clienteRequest.getNome());
+            cliente.setEmail(clienteRequest.getEmail());
+            cliente.setPassword(clienteRequest.getPassword());
+            return clienteRepository.save(cliente);
+        }).orElseThrow(() -> new RecursoNaoEncontradoException("ClienteId " + clienteId + " not found."));
+    }
+
+    @DeleteMapping("/clientes/{clienteId}")
+    public ResponseEntity<?> deleteCliente(@PathVariable Long clienteId) {
+        return clienteRepository.findById(clienteId).map(cliente -> {
+            clienteRepository.delete(cliente);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new RecursoNaoEncontradoException("ClienteId " + clienteId + " not found."));
+    }
+}
