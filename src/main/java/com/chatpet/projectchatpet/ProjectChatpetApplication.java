0cf824014e6617972e7bfb432bb7673b778fac3b
Kleber Silva <kb-max@hotmail.com>
2018-12-02 16:38:47 -0200

    Commit inicial e entidade cliente criada

---
+++ src/main/java/com/chatpet/projectchatpet/ProjectChatpetApplication.java
@@ -0,0 +1,12 @@
+package com.chatpet.projectchatpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectChatpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectChatpetApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chatpet/projectchatpet/ProjectChatpetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chatpet.projectchatpet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectChatpetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
